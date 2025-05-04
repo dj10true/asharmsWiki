dca60abf0acf24ecfb046799516dcf0bc079d88c
dj10true <106715663+dj10true@users.noreply.github.com>
2025-05-04 21:00:16 +0900

    mobile improvements

--- JS/Personal_Growth.js
+++ JS/Personal_Growth.js
@@ -1,3 +1,11 @@
+const  modal = document.querySelector('.modal');
+const modalContainer = document.querySelector('.modal-con');
+const  modalImg = document.getElementById("popupImg");
+const  captionText = document.getElementById("caption");
+const  bio = document.querySelector('.bio');
+const  closebtn = document.getElementsByClassName("close")[0];
+
+
 function Tab(e, TabName) {
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabContent");
@@ -30,3 +38,36 @@ function Close_lvl(d, lvl){
   document.getElementById(lvl).style.display = "none";
   d.currentTarget.className += "Closed";
 };
+
+
+
+
+//Imported
+const Frames = [
+  { name:"Weiss-Blaut", info:'Description: yesssssss'},
+  { name:"Tharsis", info:'Description: ROBOT'},
+  { name:"Crown", info:'Description: A massive Crown'},
+];
+
+const frameImgs = document.querySelectorAll('.frameImg').forEach(frame=>{
+    frame.addEventListener('click',()=>{
+     modalContainer.style.display = 'flex';
+     modalContainer.classList = "modal-con_opened";
+     modal.style.display = 'inline-flex';
+     modalImg.src = frame.src;
+     captionText.style.display = 'block'
+     captionText.innerHTML = frame.alt;
+
+     Frames.filter((frames)=>{
+      for (const name in frames) {
+        if(frames[name] === frame.alt){ return bio.innerHTML = frames.info } }
+      })
+     modal.scrollIntoView({behavior: "smooth"});
+     closebtn.style.display = 'block';
+    })
+})
+
+closebtn.onclick = function() { 
+  modal.style.display = "none";
+  modalContainer.classList = "modal-con_closed"
+}
